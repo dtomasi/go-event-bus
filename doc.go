c99cb9c87e59e86238860531bdd1d8e7aa94fae1
Dominik Tomasi <dominik.tomasi@gmail.com>
2021-08-28 21:25:14 +0200

    docs: add doc for go pkg

---
+++ doc.go
@@ -0,0 +1,3 @@
+// Package eventbus provides a simple event system that can be used synchronous and asynchronous
+// using channels and callbacks. It also supports subscribing via wildcards to listen to a group/unit of event channels
+package eventbus
